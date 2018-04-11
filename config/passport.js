b910a7af1840af633bb357946c4d5f2ec34aaca0
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2018-04-11 11:02:11 -0400

    have passport config use federalist env service instaed of app environment variables

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,21 @@
-const env = require('../services/environment.js')();
+const cfenv = require('cfenv');
+const appEnv = cfenv.getAppEnv();
+
+// GitHub Passport Configs
+let clientID = 'not_set';
+let clientSecret  = 'not_set';
+let callbackURL = 'not_set';
+
+const fedCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-env`);
+if (fedCreds) {
+  clientID = fedCreds.GITHUB_CLIENT_ID;
+  clientSecret = fedCreds.GITHUB_CLIENT_SECRET;
+  callbackURL = fedCreds.GITHUB_CLIENT_CALLBACK_URL;
+}
 
 module.exports = {
   github: {
-    options: {
-      clientID: env.GITHUB_CLIENT_ID || 'not_set',
-      clientSecret: env.GITHUB_CLIENT_SECRET || 'not_set',
-      callbackURL: env.GITHUB_CLIENT_CALLBACK_URL || 'not_set',
-      scope: ['user', 'repo', 'write:repo_hook'],
-    },
+    options: { clientID, clientSecret, callbackURL, scope: ['user', 'repo', 'write:repo_hook'], },
     organizations: [
       6233994,  // 18f
       14109682, // federalist-users
